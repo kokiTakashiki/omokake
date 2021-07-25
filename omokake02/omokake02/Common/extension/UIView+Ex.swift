e7d07ded5abdf0b63e14edb9354393bb99ee0a0f
koki T <bluewhitered123@gmail.com>
2021-07-26 01:50:50 +0900

    ヘルプ画面でセクションごとに枠で囲みました。

---
+++ omokake02/omokake02/Common/extension/UIView+Ex.swift
@@ -0,0 +1,42 @@
+//
+//  UIView+Ex.swift
+//  omokake02
+//
+//  Created by 武田孝騎 on 2021/07/25.
+//  Copyright © 2021 takasiki. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return self.layer.borderWidth
+        }
+        set {
+            self.layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+}
