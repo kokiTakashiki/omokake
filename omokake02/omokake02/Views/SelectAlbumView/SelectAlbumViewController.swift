cd923194dad20741ee63cb4f379f1fc743bb6e33
kokiTakeda <bluewhitered123@gmail.com>
2023-03-12 11:30:26 +0900

    add support device

--- omokake02/omokake02/Views/SelectAlbumView/SelectAlbumViewController.swift
+++ omokake02/omokake02/Views/SelectAlbumView/SelectAlbumViewController.swift
@@ -75,7 +75,7 @@ extension SelectAlbumViewController {
 // MARK: - Extension Private Method
 extension SelectAlbumViewController {
     // TODO: チップの性能ごとに自動判定したい。
-    // このサイトを参考に分岐　https://volx.jp/iphone-antutu-benchmark
+    // このサイトを参考に分岐　https://www.antutu.com/en/ranking/ios1.htm
     private func deviceMaxParts(_ note: AlbumInfo) {
         let device = Device.current
         print("[SelectAlbumViewController] deviceMaxParts \(device)")
@@ -83,7 +83,11 @@ extension SelectAlbumViewController {
         case .iPhoneXR, .iPhoneXSMax, .iPhoneXS, .iPhoneSE2:
             partsAlertAndPresent(note, maxParts: 300)
 
-        case .iPhone11, .iPhone11ProMax, .iPhone11Pro, .iPhone12Pro, .iPhone12, .iPhone12ProMax:
+        case .iPhone11, .iPhone11ProMax, .iPhone11Pro,
+                .iPhone12Pro, .iPhone12, .iPhone12ProMax, .iPhone12Mini,
+                .iPhoneSE3, .iPhone13Mini, .iPhone13,
+                .iPhone14, .iPhone14Plus, .iPhone13Pro,
+                .iPhone13ProMax, .iPhone14ProMax, .iPhone14Pro:
             partsAlertAndPresent(note, maxParts: 400)
 
         default:
