f397e267c42296429449f4caac7bf9ff4bd60ef0
kokiTakeda <bluewhitered123@gmail.com>
2023-03-12 11:23:51 +0900

    change back button layout and add back se

--- omokake02/omokake02/Views/HelpView/HelpViewController.swift
+++ omokake02/omokake02/Views/HelpView/HelpViewController.swift
@@ -8,12 +8,25 @@
 
 import UIKit
 import SwiftUI
+import Combine
 
 class HelpViewController: UIViewController {
+    enum BackType {
+    case button
+    case popGesture
+    }
+    private let audio = PlayerController.shared
+    private let backTypeSubject = PassthroughSubject<BackType, Never>()
+    private var cancels = Set<AnyCancellable>()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let chevronLeftImage: UIImage? = UIImage(systemName: "chevron.left")
+        let backButton = UIBarButtonItem(image: chevronLeftImage, style: .plain, target: self, action: #selector(onTapBackButton(_:)))
+        navigationItem.leftBarButtonItem = backButton
         self.navigationController?.navigationBar.tintColor = .lightGray
+        self.navigationController?.interactivePopGestureRecognizer?.delegate = self
 
         let hostingController: UIHostingController = UIHostingController(rootView: HelpView())
         self.addChild(hostingController)
@@ -25,5 +38,35 @@ class HelpViewController: UIViewController {
         hostingController.view.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 0).isActive = true
         hostingController.view.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0).isActive = true
         hostingController.view.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+
+        // MARK: Bind
+        backTypeSubject
+            .scan((BackType.popGesture, BackType.popGesture)) { ($0.1, $1) }
+            .sink { [weak self] previous, current in
+                if previous == current {
+                    self?.audio.play(effect: Audio.EffectFiles.transitionDown)
+                }
+        }
+        .store(in: &cancels)
+    }
+
+    @objc
+    func onTapBackButton(_ sender: UIBarButtonItem) {
+        backTypeSubject.send(.button)
+        audio.play(effect: Audio.EffectFiles.transitionDown)
+        navigationController?.popViewController(animated: true)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        backTypeSubject.send(.popGesture)
     }
 }
+
+// MARC: UIGestureRecognizerDelegate
+extension HelpViewController: UIGestureRecognizerDelegate {}
