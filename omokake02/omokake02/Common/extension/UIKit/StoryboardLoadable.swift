4e07c9cc8efdb621826dd340f2fa2f1f735c9b28
kokiTakeda <bluewhitered123@gmail.com>
2023-04-25 01:23:43 +0900

    検証用StoryboardLoadable

---
+++ omokake02/omokake02/Common/extension/UIKit/StoryboardLoadable.swift
@@ -0,0 +1,35 @@
+//
+//  StoryboardLoadable.swift
+//  omokake02
+//
+//  Created by 武田孝騎 on 2023/04/25.
+//  Copyright © 2023 takasiki. All rights reserved.
+//
+
+import UIKit
+
+public protocol StoryboardLoadable {
+    
+    static var identifier: String { get }
+    
+    static var storyboard: UIStoryboard { get }
+    
+    static func load<T: UIViewController>(creator: @escaping ((NSCoder) -> T?)) -> T
+}
+
+// MARK: Default implementations
+public extension StoryboardLoadable {
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+    
+    static var storyboard: UIStoryboard {
+        return UIStoryboard(name: String(describing: self), bundle: nil)
+    }
+    
+    static func load<T: UIViewController>(creator: @escaping ((NSCoder) -> T?)) -> T {
+        storyboard.instantiateViewController(identifier: identifier,
+                                             creator: creator)
+    }
+}
