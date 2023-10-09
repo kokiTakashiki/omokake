732b5e462caae22954d9f854451a7b265b417b36
takedatakashiki <takedatakashiki@takedatakashikinoMacBook-Pro.local>
2023-10-09 20:15:46 +0900

    renderingMode original

--- omokake02/omokake02/Views/MenuView/MenuView.swift
+++ omokake02/omokake02/Views/MenuView/MenuView.swift
@@ -102,6 +102,7 @@ struct MenuView: View {
                 } label: {
                     Image("buttonKakera")
                         .resizable()
+                        .renderingMode(.original)
                         .frame(width: Self.buttonWidth, height: Self.buttonWidth)
                         .opacity(animationState.kakeraOpacity)
                 }
@@ -113,6 +114,7 @@ struct MenuView: View {
                 } label: {
                     Image("buttonThumbnail")
                         .resizable()
+                        .renderingMode(.original)
                         .frame(width: Self.buttonWidth, height: Self.buttonWidth)
                         .opacity(animationState.thumbnailOpacity)
                 }
@@ -125,6 +127,7 @@ struct MenuView: View {
                 } label: {
                     Image("buttonSKakera")
                         .resizable()
+                        .renderingMode(.original)
                         .frame(width: Self.buttonWidth, height: Self.buttonWidth)
                         .opacity(animationState.sKakeraOpacity)
                 }
